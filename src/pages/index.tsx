207e4aaab6852e6b0eb0bb74cad9408b3dedb823
spreadmycode <spreadmycode@outlook.com>
2021-11-09 21:52:26 -0800

    some text fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -301,7 +301,7 @@ const Home = () => {
                   <img src={'/images/benefits2.png'} className="image-benefit" />
               </div>
               <h5 className="panel-why-desc-title">PW RAFFLE GAMES</h5>
-              <p className="panel-why-desc-text">PW is your lucky NFTicket to join the Raffle Draw and win Exciting Prizes (up to 60 SOL in value). 12 Raffle Games will be played during 12 upcoming months. 1 PW = 1 Entry.</p>
+              <p className="panel-why-desc-text">PW is your lucky NFTicket to join the Raffle Draw and win Exciting Prizes (up to 60 SOL in value). 12 Raffle Games will be played during 12 months from minting. 1 PW = 12 Entries.</p>
             </div>
 
             <div className="md:col-span-1 panel-benefit flex flex-col space-y-2 p-5 panel-why-border-radius">
