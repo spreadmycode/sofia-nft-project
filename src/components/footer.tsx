e71a9075232735a6e1114bf29d1b1ecdc034f97d
spreadmycode <spreadmycode@outlook.com>
2021-12-23 03:21:02 -0800

    rarity sniper logo added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,17 @@ import Link from 'next/link'
 
 const Footer: React.FC = () => {
   return <div className="flex bg-black justify-center py-10">
-    <div className="flex flex-col space-y-3">
+    <div className="flex flex-col space-y-4">
+      <div className="w-full flex justify-center items-center cursor-pointer">
+        <Link href="/">
+          <img src={'/images/logo_footer.png'} />
+        </Link>
+      </div>
+
       <div className="w-full flex justify-center items-center">
-        <img src={'/images/logo_footer.png'} />
+        <a href="https://raritysniper.com/" target="_blank" className="inline-flex text-center justify-center items-center">
+          <img src={'/images/rarity_logo/logo3-white.png'} width="40%" />
+        </a>
       </div>
 
       <div className="grid grid-cols-4 gap-6 px-10">
