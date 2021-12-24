311ed0602e677d1c48ecf03f3fc13dabadba1726
spreadmycode <spreadmycode@outlook.com>
2021-12-24 12:08:14 -0800

    mint tutorial prepaired

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,6 @@
-export const AFFILIATION_CODE_LEN = 6;
-export const AFFILIATION_WEIGHT = 9.0; // 10% will go affiliator
-export const NORMALSALE_MAX_NFT_HOLD_COUNT = 5;
-export const PRESALE_MAX_NFT_HOLD_COUNT = 4;
-export const PRESALE_SOLD_LIMIT_COUNT = 10;
-export const OWNER_AFFLIATION_CODE = "000001"
+export const AFFILIATION_CODE_LEN           = 6;
+export const AFFILIATION_WEIGHT             = 9.0; // 10% will go affiliator
+export const NORMALSALE_MAX_NFT_HOLD_COUNT  = 5;
+export const PRESALE_MAX_NFT_HOLD_COUNT     = 4;
+export const PRESALE_SOLD_LIMIT_COUNT       = 1500;
+export const OWNER_AFFLIATION_CODE          = "000001"
