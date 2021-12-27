63aeea59370ce84fb3bd6fb1c56aa3250d1b6675
spreadmycode <spreadmycode@outlook.com>
2021-12-27 09:19:57 -0800

    pre-sale first step:updated cors backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -231,7 +231,6 @@ const Home = () => {
 
       <Head>
         <title>Panda Warriors</title>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
         <meta name="description" content="Panda Warriors is a collection of 10,000 generated NFTs, living on the Solana Blockchain, each asset is hand-drawn from scratch by a talented group of artists. Every art piece present on our website is hand-drawn by us. Each Panda Warrior is your lucky NFTicket, that will grow in value every phase of the Panda Warriors project development. Be a part of our journey full of adventures and fun." />
         <link rel="icon" href="/icon.png" />
       </Head>
